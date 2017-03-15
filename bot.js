5f386af986bb7280e3c5cb05620892de0928aa81
Mohamed Serag <mohamed.serag@outlook.com>
2017-03-15 18:31:18 +0200

    log hello message

--- bot.js
+++ bot.js
@@ -35,6 +35,7 @@ var read = function (sender, message, reply) {
 	if (message === 'hello') {
 		// Let's reply back hello
 		message = 'Hello yourself! I am a chat bot. You can say "show me pics of corgis"'
+		console.log('inside hello')
 		reply(sender, message)
 	} else {
 		// Let's find the user
